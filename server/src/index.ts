60160d12958fb3e497dc65a99effd171e23bd391
cliffhall <cliff@futurescale.com>
2025-05-13 16:21:26 -0400

    Add www-authenticate to Access-Control-Expose-Headers list for responses. This fixes #168

--- server/src/index.ts
+++ server/src/index.ts
@@ -44,7 +44,7 @@ const { values } = parseArgs({
 const app = express();
 app.use(cors());
 app.use((req, res, next) => {
-  res.header("Access-Control-Expose-Headers", "mcp-session-id");
+  res.header("Access-Control-Expose-Headers", ["mcp-session-id", "www-authenticate"]);
   next();
 });
 
