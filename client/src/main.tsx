21995df1782cd3e35c4f0de3a2d21a6057f351b9
olaservo <olahungerford@gmail.com>
2025-12-08 09:56:22 -0700

    feat: implement toast notification system with sonner (UI-17)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { Toaster } from './components/ui/sonner';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Toaster />
     </BrowserRouter>
   </StrictMode>
 );
